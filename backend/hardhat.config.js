7cfbc551cc1f17b6ab123bdb4eaff34829965b28
MohsinAliSolangi <mohsinali.solangi789@gmail.com>
2024-04-07 01:59:20 +0500

    changes in code
    changes in code

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,32 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
-//require('hardhat-abi-exporter');
 require('dotenv').config({path: __dirname+'/.env'})
 require("@nomiclabs/hardhat-etherscan");
 require('hardhat-contract-sizer');
-// require('hardhat-ethernal');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// console.log(process.env.ALCHEMY_API)
-// console.log(process.env.privateKey)
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-// const ALCHEMY_API_KEY = `
-
-// const privateKey = `669a00a5dcee6b12e70ec23b4a793b14bcb38a0f657ce29ada80b578e14743a7`
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -81,11 +58,9 @@ module.exports = {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: "GC3A7BY9VEP5R54SDEGT3EQVE9G4AC13PV"
+    apiKey: ""
   },
   mocha: {
     timeout: 1000000
   }
 };
-// npx hardhat verify --constructor-args ./arguments.js --network rinkeby 0x8Eb6B4D40D35243352aBAD59BFDB27a161F3Bdc1
-//npx hardhat verify --network rinkeby 0x8Eb6B4D40D35243352aBAD59BFDB27a161F3Bdc1 "0x3B2FA3fB4c7eD3bC495F276DC60782b635bB04d9" "0x3B2FA3fB4c7eD3bC495F276DC60782b635bB04d9"
